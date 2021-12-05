fe08c10549992aa3d1c1aba6d7931ac7a5e91e4a
jonaszilke <jonas.zilke@gmail.com>
2021-12-05 11:14:09 +0100

    finish day 4

--- day4/solution.py
+++ day4/solution.py
@@ -8,7 +8,7 @@ def read_file(file_name: str) -> List[str]:
 
 def get_data(file_name: str):
     lines = read_file(file_name)
-    numbers = lines[0].split(',')
+    numbers = list(map(int, lines[0].split(',')))
     boards = [[]]
     board_number = 0
     for line in lines[2:]:
@@ -20,22 +20,59 @@ def get_data(file_name: str):
     return numbers, boards
 
 
-def exercise1(numbers: List[int], boards: List[List[List[int]]]) -> int:
-    for n in numbers:
+def is_finished(numbers, board):
+    for row in board:
+        if all(item in numbers for item in row):
+            return True
+    for column in list(map(list, zip(*board))):
+        if all(item in numbers for item in column):
+            return True
+    return False
+
+
+def find_first_board(numbers: List[int], boards: List[List[List[int]]]):
+    for i in range(1, len(numbers)):
         for board in boards:
-            pass
-    return 0
+            if is_finished(numbers[:i], board):
+                return numbers[:i], board
 
 
-def exercise2(puzzle_input: List[str]) -> int:
-    return 1
+def exercise1(numbers: List[int], boards: List[List[List[int]]]) -> int:
+    used_numbers, board = find_first_board(numbers, boards)
+    for num in used_numbers:
+        for row in board:
+            if num in row:
+                row.remove(num)
+    return sum(sum(board, [])) * used_numbers[-1]
+
+
+def find_last_board(numbers: List[int], boards: List[List[List[int]]]):
+    cboards = boards.copy()
+    i = 1
+    while len(cboards) > 1:
+        finished = []
+        for b in cboards:
+            if is_finished(numbers[:i], b):
+                finished.append(b)
+        for b in finished:
+            cboards.remove(b)
+        i += 1
+    return numbers[:i], cboards[0]
+
+
+def exercise2(numbers: List[int], boards: List[List[List[int]]]) -> int:
+    used_numbers, board = find_last_board(numbers, boards)
+    for num in used_numbers:
+        for row in board:
+            if num in row:
+                row.remove(num)
+    return sum(sum(board, [])) * used_numbers[-1]
 
 
 if __name__ == "__main__":
-    # test_data = get_data("example_data")
-    # assert exercise1(test_data) == 198
-    # assert exercise2(test_data) == 230
-
-    data = get_data("data")
-    print(f"Exercise 1: {exercise1(data)}")
-    print(f"Exercise 2: {exercise2(data)}")
+    numbers, boards = get_data("example_data")
+    assert exercise1(numbers, boards) == 4512
+    assert exercise2(numbers, boards) == 1924
+    numbers, boards = get_data("data")
+    print(f"Exercise 1: {exercise1(numbers, boards)}")
+    print(f"Exercise 2: {exercise2(numbers, boards)}")
